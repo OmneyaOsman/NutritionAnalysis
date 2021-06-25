284d2c67ba23e0297a80be0e54ebd9636c214d95
Omni <omneya.osman@outlook.com>
2021-06-25 21:24:57 +0200

    Initial commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,18 @@
+repositories {
+    jcenter()
+    google()
+    mavenCentral()}
+
+plugins {
+    `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.android.tools.build:gradle:4.2.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+
+    implementation(gradleApi())
+}
+
