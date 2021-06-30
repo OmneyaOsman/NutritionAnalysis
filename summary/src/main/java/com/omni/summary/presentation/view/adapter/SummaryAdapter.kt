35cba54309d3a654e178d061a859dafdbe33ff2d
Omni <omneya.osman@outlook.com>
2021-06-30 13:42:29 +0200

    add header

--- summary/src/main/java/com/omni/summary/presentation/view/adapter/SummaryAdapter.kt
+++ summary/src/main/java/com/omni/summary/presentation/view/adapter/SummaryAdapter.kt
@@ -5,31 +5,55 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.devexy.summary.databinding.IngredientItemHeaderBinding
 import com.devexy.summary.databinding.ListItemIngredientBinding
 import com.omni.analysis_shared_data.domain.entity.IngredientEntity
 
 class SummaryAdapter(private val list: List<IngredientEntity>) :
-    RecyclerView.Adapter<SummaryAdapter.SummaryViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
-    class SummaryViewHolder(private val binding: ListItemIngredientBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(ingredientEntity: IngredientEntity) {
-            binding.calories.text = ingredientEntity.calories
-            binding.ingredient = ingredientEntity
-            binding.executePendingBindings()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            VIEW_TYPE_ITEM ->
+                SummaryViewHolder(ListItemIngredientBinding.inflate(layoutInflater, parent, false))
+            VIEW_TYPE_HEADER -> {
+                SummaryHeaderViewHolder(
+                    IngredientItemHeaderBinding.inflate(
+                        layoutInflater,
+                        parent,
+                        false
+                    )
+                )
+            }
+            else -> throw IllegalStateException("Settings view holder for type $viewType")
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SummaryViewHolder {
-        LayoutInflater.from(parent.context).let {
-            ListItemIngredientBinding.inflate(it, parent, false)
-        }.also { return SummaryViewHolder(it) }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is SummaryViewHolder ->
+                holder.bind(list[position-1])
+
+            is SummaryHeaderViewHolder -> {
+            }
+            else -> throw IllegalStateException("Settings view holder for type ${holder.itemViewType}")
+        }
     }
 
+    override fun getItemViewType(position: Int): Int =
+        when (position) {
+            0 -> VIEW_TYPE_HEADER
+            else -> VIEW_TYPE_ITEM
 
-    override fun getItemCount(): Int = list.count()
-    override fun onBindViewHolder(holder: SummaryViewHolder, position: Int) {
-        holder.bind(list[position])
+        }
+
+    override fun getItemCount(): Int = list.size + 1
+
+    companion object {
+        private const val VIEW_TYPE_HEADER = 0
+        private const val VIEW_TYPE_ITEM = 1
     }
 }
---
+++ summary/src/main/java/com/omni/summary/presentation/view/adapter/SummaryViewHolder.kt
@@ -0,0 +1,14 @@
+package com.omni.summary.presentation.view.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.devexy.summary.databinding.ListItemIngredientBinding
+import com.omni.analysis_shared_data.domain.entity.IngredientEntity
+
+class SummaryViewHolder(private val binding: ListItemIngredientBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(ingredientEntity: IngredientEntity) {
+        binding.calories.text = ingredientEntity.calories
+        binding.ingredient = ingredientEntity
+        binding.executePendingBindings()
+    }
+}
